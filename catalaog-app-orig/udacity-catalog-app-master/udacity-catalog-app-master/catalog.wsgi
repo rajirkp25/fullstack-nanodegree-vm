a94e2c660f44f9975447126dee6ee70888aa8648
Rama, Raji (RR5924) <rr5924@att.com>
2019-05-12 23:31:06 -0500

    Changes committed by Raji (RR5924) for final changes

--- catalaog-app-orig/udacity-catalog-app-master/udacity-catalog-app-master/catalog.wsgi
+++
@@ -1,5 +0,0 @@
-import sys
- 
-sys.path.append('/var/www/catalog')
- 
-from server import app as application
--- udacity-catalog-app-master/static/functions.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	$('select#categories').find('option[value="'+$('input#category_id_helper').val()+'"]').attr('selected', true);
-	$('select').material_select();
-});
